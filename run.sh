2c3215dcd2069d559dd9a2d83cdc01e77d13af03
Sihan0229 <sihangao2004@gmail.com>
2025-03-06 23:17:13 +0800

    run

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-python3 run_pipeline.py --in_dir='/root/autodl-tmp/dhcp-dl-neonatal-main/datasets_2/' \
-                       --out_dir='/root/autodl-tmp/dhcp-dl-neonatal-main/results/' \
+python3 run_pipeline.py --in_dir='/root/autodl-tmp/hcp_gcn/datasets_2/' \
+                       --out_dir='/root/autodl-tmp/hcp_gcn/results/' \
                        --T2='_T2w.nii.gz' \
                        --T1='_T1w.nii.gz' \
                        --sphere_proj='fs' \
