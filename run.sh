631b82c75fd4cf7af096f6bee7f0770adfced22b
Sihan0229 <sihangao2004@gmail.com>
2025-03-06 23:12:40 +0800

    others

---
+++ run.sh
@@ -0,0 +1,6 @@
+python3 run_pipeline.py --in_dir='/root/autodl-tmp/dhcp-dl-neonatal-main/datasets_2/' \
+                       --out_dir='/root/autodl-tmp/dhcp-dl-neonatal-main/results/' \
+                       --T2='_T2w.nii.gz' \
+                       --T1='_T1w.nii.gz' \
+                       --sphere_proj='fs' \
+                       --device='cuda:0'
